84a969266eb9c801313403a383c0f523229eb995
kaspnou <kaspnou@greeny.cs.tlu.ee>
2016-09-22 13:32:48 +0300

    uuendasin

--- login3.php
+++ login3.php
@@ -1,10 +1,15 @@
 <?php
 
+	require("../../config.php");
+	
+	//<?php echo $m > ---- <?== $m >
+
 	//var_dump($_GET);
 	//echo "<br>";
 	//var_dump($_POST);
 	
 	$signupEmailError = "";
+	$signupEmail = "";
 	
 	//kas on üldse olemas
 	if (isset ($_POST["signupEmail"])) {
@@ -12,6 +17,9 @@
 		if (empty($_POST["signupEmail"])) {
 			//oli tõesti tühi
 			$signupEmailError = "See väli on kohustuslik";
+		} else {
+			// kõik korras, emal ei ole tühi ja on olemas
+			$signupEmail = $_POST["signupEmail"];
 		}
 	}
 
@@ -81,7 +89,65 @@
 			$postiindeksError = "See väli on kohustuslik";
 		}
 	}
-
+	$gender = "";
+	if(isset($_POST["gender"])) {
+		if(!empty($_POST["gender"])){
+			
+			//on olemas ja ei ole tühi
+			$gender = $_POST["gender"];
+		}
+	}
+	
+	if (isset($_POST["signupEmail"]) &&
+		isset($_POST["signupPassword"]) &&
+		isset($_POST["nr"]) &&
+		isset($_POST["cscnr"]) &&
+		isset($_POST["tanav"]) &&
+		isset($_POST["linn/vald"]) &&
+		isset($_POST["postiindeks"]) &&
+		$tanavError == "" &&
+		$linnvaldError == "" &&
+		$postiindeksError == "" &&
+		$nrError == "" &&
+		$cscnrError == "" &&
+		$signupEmailError == "" && 
+		empty($signupPasswordError)
+		
+		) {
+		
+		// ühtegi viga ei ole, kõik vajalik olemas.
+		echo "salvestan...<br>";
+		echo "email ".$signupEmail."<br>";
+		echo "parool ". $_POST["signupPassword"]."<br>";
+		
+		$password = hash("sha512", $_POST["signupPassword"]);
+		
+		echo "räsi ".$password."<br>";
+		
+		//ühendus
+		$database = "if16_kaspnou";
+		$mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
+		
+		//käsk
+		$stmt = $mysqli->prepare("INSERT INTO user_sample (email, password) VALUES (?, ?)");
+		
+		echo $mysqli->error;
+		
+		// s - string
+		// i - int
+		// d - decimal/double
+		// iga küsimärgi jaoks üks täht, mis tüüpi on
+		$stmt->bind_param("ss", $signupEmail, $password );
+		
+		if($stmt->execute() ) {
+			echo "salvestamine õnnestus";
+		} else 	{
+			echo "error ".$stmt->error;
+		}	
+		
+		
+	}
+	
 ?>
 <!DOCTYPE html>
 <html>
@@ -112,14 +178,30 @@
 		
 		<form method="POST">
 		
-			<input placeholder="E-mail" name="signupEmail" type="email"> <?php echo $signupEmailError; ?>
+			<input placeholder="E-mail" name="signupEmail" type="email" value="<?php echo $signupEmail; ?>"> <?php echo $signupEmailError; ?>
 			
 			<br><br>
 			
 			<input placeholder="Parool" name="signupPassword" type="password"> <?php echo $signupPasswordError; ?>
-			
-			
 			<br><br>
+			<?php if ($gender == "male") { ?>
+				<input type="radio" name="gender" value="male" checked> Mees<br>
+			<?php } else { ?>
+				<input type="radio" name="gender" value="male" > Mees<br>
+			<?php } ?>
+			
+			<?php if ($gender == "female") { ?>
+				<input type="radio" name="gender" value="female" checked> Naine<br>
+			<?php } else { ?>
+				<input type="radio" name="gender" value="female"> Naine<br>
+			<?php } ?>
+			
+			<?php if ($gender == "other") { ?>
+				<input type="radio" name="gender" value="other" checked> Muu<br><br>
+			<?php } else { ?>
+				<input type="radio" name="gender" value="other"> Muu<br><br>
+			<?php } ?>
+			
 			Krediitkaardiinfo
 			<br><br>
 			<input placeholder="Krediitkaardi nr." name="nr" type="number"> <?php echo $nrError; ?>
